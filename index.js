eeff13abcc338a3a84c3acc824e69cbd00197fc3
dfghjkcvbnmnbvcx <japdcweondy@hotmail.com>
2024-03-16 17:59:38 +0700

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const courses = [
+    "Mathematics",
+    "Science",
+    "History",
+    "English",
+    "Computer Science"
+  ];
+  
+  console.log("Courses:");
+  courses.forEach(course => {
+    console.log(course);
+  });
+  
